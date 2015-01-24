c02f0263c6e6fc43b8d3fc59d80d5794f69c3b98
Bill Lazar <blazar@gmail.com>
2015-01-24 14:52:58 -0800

    Migrate Rooms to new migration syntax

--- priv/repo/migrations/20150116045636_initial_rooms_create.exs
+++ priv/repo/migrations/20150116045636_initial_rooms_create.exs
@@ -2,16 +2,16 @@ defmodule FdEvents.Repo.Migrations.InitialRoomsCreate do
   use Ecto.Migration
 
   def up do
-    "CREATE TABLE rooms( \
-      id serial primary key, \
-      name varchar(255), \
-      capacity integer, \
-      active boolean, \
-      created_at timestamp, \
-      updated_at timestamp)"
+    create table(:rooms) do
+      add :name, :string, size: 255
+      add :capacity, :integer
+      add :active, :boolean
+      
+      timestamps
+    end
   end
 
   def down do
-    "DROP TABLE rooms"
+    drop table(:rooms)
   end
 end
